d594b2e0e18187fab23aadc19fd3c0d22f377fb1
Bloxybeans <quackahh123@gmail.com>
2025-02-03 16:42:13 -0700

    BLE Spam Android
    As a disclaimer, this is not my project, it was created by simondankelmann at https://github.com/simondankelmann/Bluetooth-LE-Spam

---
+++ app/src/main/java/de/simon/dankelmann/bluetoothlespam/Helpers/QueueHandlerHelpers.kt
@@ -0,0 +1,36 @@
+package de.simon.dankelmann.bluetoothlespam.Helpers
+
+import android.content.Context
+import android.util.Log
+import androidx.preference.PreferenceManager
+import de.simon.dankelmann.bluetoothlespam.R
+
+
+class QueueHandlerHelpers {
+    companion object {
+        private const val _logTag = "QueueHandlerHelpers"
+
+        fun getInterval(context: Context): Int {
+            // Get from Settings, if present
+            val preferences = PreferenceManager.getDefaultSharedPreferences(context).all
+            val prefKey =
+                context.resources.getString(R.string.preference_key_interval_advertising_queue_handler)
+            preferences.forEach {
+                if (it.key == prefKey) {
+                    val intervalString = it.value as? String
+                    if (intervalString != null) {
+                        try {
+                            val parsedInterval = intervalString.toInt()
+                            if (parsedInterval > 0) {
+                                return parsedInterval
+                            }
+                        } catch (e: NumberFormatException) {
+                            Log.d(_logTag, "Invalid interval specified: $intervalString")
+                        }
+                    }
+                }
+            }
+            return 1000
+        }
+    }
+}
